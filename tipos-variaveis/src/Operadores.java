89600cc76bf94122c554d3787c4a9da8856eb196
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-03-23 00:29:59 -0300

    Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+        numero = -numero;
+        System.out.println(numero);
+        numero = numero * -1; // pra tornar o numero da variavel positiva
+        System.out.println(numero);
+
+
+    }
+}
